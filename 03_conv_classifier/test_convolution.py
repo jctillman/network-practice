cfc6c03e08cc5ee286b7009e92ce92665c538199
James Tillman <james.tillman.7@gmail.com>
2019-04-20 15:00:05 -0400

    Improving test by a bunch.

--- 03_conv_classifier/test_convolution.py
+++ 03_conv_classifier/test_convolution.py
@@ -5,214 +5,63 @@ import numpy as np
 from convolution import ConvolutionMult, Convolution, MaxPool
 from loss import mean_squared_loss
 
-
-def generic_test_Convolution_dimensions(conv_cls):
-
-    for _ in range(10):
-        batch_size = randint(1, 4)
-        width = randint(5, 10)
-        height = randint(5, 10)
-        input_c = randint(1, 3)
-        rand_input = rand(batch_size, width, height, input_c)
-
-        kernel = 3
-        output_c = randint(1, 5)
-        conv = conv_cls((width, height, input_c), kernel, output_c)
-
-        result = conv.forward(rand_input)
-
-        assert result.shape[0] == batch_size
-        assert result.shape[1] == width - int(kernel - 1)
-        assert result.shape[2] == height - int(kernel - 1)
-        assert result.shape[3] == output_c
-
-def module_test_matrices(times):
-
-    for _ in range(times):
-        batch_size = randint(low=1, high=3)
-        
-        width = randint(low=5, high=13)
-        height = randint(low=5, high=13)
-        input_c = randint(low=1, high=3)
-        
-        output_c = randint(1, 4)
-
-        inp = rand(batch_size, width, height, input_c) * 0.5 + 0.25
-
-        out = rand(batch_size, width - 2, height - 2, output_c) * 0.5 + 0.25
-        
-        yield batch_size, width, height, input_c, output_c, inp, out
-
+from utils_for_tests import (
+    same_generator,
+    alt_generator,
+    generic_test_module_optimize,
+    generic_test_module_derivative_amount)
 
 def generic_test_Convolution_optimize(conv_cls):
-    
-    LR = 0.0001
-    tries = 200
-    failures = 0
-    for bs, w, h, inp_c, out_c, inp, truth in module_test_matrices(tries):
-       
-        model = conv_cls((w, h, inp_c), 3, out_c)
-        outputs = model.forward(inp)
-        old_loss, derivative = mean_squared_loss(prediction=outputs, truth=truth)
-        model.backward(derivative)
-        model.optimize(LR)
 
-        new_outputs = model.forward(inp)
-        new_loss, derivative = mean_squared_loss(prediction=new_outputs, truth=truth)
-        if new_loss >= old_loss:
-            failures = failures + 1
-    
-    assert failures <= tries / 5
+    def shape_generator():
+        return (
+            [[4,12,12,2],[0,0,0,0]],
+            [[4,10,10,2],[0,0,0,0]]
+        )
 
+    def init_fnc(input_shape, output_shape):
+        return conv_cls(input_shape[1:], 3, output_shape[-1])
+
+    generic_test_module_optimize(init_fnc, mean_squared_loss, shape_generator)
+    
 
 def generic_test_Convolution_derivative_amount(conv_cls):
-    
-    LR = 0.0000001
-    tries = 100
-    failures = 0
-    skips = 0
-    for bs, w, h, inp_c, out_c, inputs, truth in module_test_matrices(tries):
-       
-        model = conv_cls((w, h, inp_c), 3, out_c)
-        outputs = model.forward(inputs)
-        old_loss, derivative = mean_squared_loss(prediction=outputs, truth=truth)
-        input_deriv = model.backward(derivative)
-        
-        indiv_w = randint(0, w)
-        indiv_h = randint(0, h)
-        spot = randint(0, inp_c)
 
-        # Algo breaks down where derivative is mostly flat
-        in_d = input_deriv
-        if np.any(np.abs(in_d[:,indiv_w,indiv_h,spot]) < 0.002):
-            skips += 1
-            continue
+    def shape_generator():
+        return (
+            [[1,12,12,2],[0,0,0,0]],
+            [[1,10,10,3],[0,0,0,0]]
+        )
 
-        for bs_l in range(bs):
-            change_amount = (1 / input_deriv[bs_l][indiv_w][indiv_h][spot]) * LR
-            inputs[bs_l][indiv_w][indiv_h][spot] = (
-                inputs[bs_l][indiv_w][indiv_h][spot] - change_amount)
+    def init_fnc(input_shape, output_shape):
+        return conv_cls(input_shape[1:], 3, output_shape[-1])
 
-
-        new_outputs = model.forward(inputs)
-        new_loss, derivative = mean_squared_loss(prediction=new_outputs, truth=truth)
-        
-        assert np.isclose(
-            (old_loss - new_loss) * (1 / LR),
-            1 * bs,
-            atol=0.02) or (old_loss == 0.0 and new_loss == 0.0)
-    
-    assert skips < tries / 3
-
-
-def generic_test_Convolution_derivative_amount(conv_cls):
-    
-    LR = 0.0000001
-    tries = 100
-    failures = 0
-    skips = 0
-    for bs, w, h, inp_c, out_c, inputs, truth in module_test_matrices(tries):
-       
-        model = conv_cls((w, h, inp_c), 3, out_c)
-        outputs = model.forward(inputs)
-        old_loss, derivative = mean_squared_loss(prediction=outputs, truth=truth)
-        input_deriv = model.backward(derivative)
-        
-        indiv_w = randint(0, w)
-        indiv_h = randint(0, h)
-        spot = randint(0, inp_c)
-
-        # Algo breaks down where derivative is mostly flat
-        in_d = input_deriv
-        if np.any(np.abs(in_d[:,indiv_w,indiv_h,spot]) < 0.002):
-            skips += 1
-            continue
-
-        for bs_l in range(bs):
-            change_amount = (1 / input_deriv[bs_l][indiv_w][indiv_h][spot]) * LR
-            inputs[bs_l][indiv_w][indiv_h][spot] = (
-                inputs[bs_l][indiv_w][indiv_h][spot] - change_amount)
-
-
-        new_outputs = model.forward(inputs)
-        new_loss, derivative = mean_squared_loss(prediction=new_outputs, truth=truth)
-        
-        assert np.isclose(
-            (old_loss - new_loss) * (1 / LR),
-            1 * bs,
-            atol=0.02) or (old_loss == 0.0 and new_loss == 0.0)
-    
-    assert skips < tries / 3
-
+    generic_test_module_derivative_amount(init_fnc, mean_squared_loss, shape_generator)
 
 
 def test_Convolution_vanilla():
-    generic_test_Convolution_dimensions(ConvolutionMult)
     generic_test_Convolution_optimize(ConvolutionMult)
     generic_test_Convolution_derivative_amount(ConvolutionMult)
 
+
 def test_Convolution():
-    generic_test_Convolution_dimensions(Convolution)
     generic_test_Convolution_optimize(Convolution)
     generic_test_Convolution_derivative_amount(Convolution)
 
 
-def module_test_matrices_pool(times):
-
-    for _ in range(times):
-        batch_size = randint(low=1, high=3)
-        
-        width = 12
-        height = 12
-        input_c = randint(low=1, high=3)
-        
-        size = randint(low=1, high=4)
-
-        inp = rand(batch_size, width, height, input_c) * 0.5 + 0.25
-
-        out = rand(batch_size, int(width / size), int(height/ size), input_c) * 0.5 + 0.25
-        
-        yield batch_size, width, height, input_c, inp, out, size
-
-
 def generic_test_Pool_derivative_amount(pool):
-    
-    LR = 0.0000001
-    tries = 100
-    failures = 0
-    skips = 0
-    for bs, w, h, input_c, inp, truth, size in module_test_matrices_pool(tries):
-       
-        model = pool(size)
-        outputs = model.forward(inp)
-        old_loss, derivative = mean_squared_loss(prediction=outputs, truth=truth)
-        input_deriv = model.backward(derivative)
-        
-        indiv_w = randint(0, w / size)
-        indiv_h = randint(0, h / size)
-        spot = randint(0, input_c)
 
-        # Algo breaks down where derivative is mostly flat
-        in_d = input_deriv
-        if np.any(np.abs(in_d[:,indiv_w,indiv_h,spot]) < 0.002):
-            skips += 1
-            continue
+    def shape_generator():
+        return (
+            [[1,12,12,2],[0,0,0,0]],
+            [[1,6,6,2],[0,0,0,0]]
+        )
 
-        for bs_l in range(bs):
-            change_amount = (1 / input_deriv[bs_l][indiv_w][indiv_h][spot]) * LR
-            inp[bs_l][indiv_w][indiv_h][spot] = (
-                inp[bs_l][indiv_w][indiv_h][spot] - change_amount)
+    def init_fnc(input_shape, output_shape):
+        return pool(2)
 
+    generic_test_module_derivative_amount(init_fnc, mean_squared_loss, shape_generator, ratio_skips=0.05)
 
-        new_outputs = model.forward(inp)
-        new_loss, derivative = mean_squared_loss(prediction=new_outputs, truth=truth)
-        
-        assert np.isclose(
-            (old_loss - new_loss) * (1 / LR),
-            1 * bs,
-            atol=0.02) or (old_loss == 0.0 and new_loss == 0.0)
-    
-    assert skips < tries / 1.5
 
 def test_MaxPool():
 
--- 03_conv_classifier/test_graph_components.py
+++ 03_conv_classifier/test_graph_components.py
@@ -1,6 +1,12 @@
 
 import numpy as np
-from numpy.random import random, randint, rand
+from numpy.random import random, rand
+
+from utils_for_tests import (
+    same_generator,
+    alt_generator,
+    generic_test_module_optimize,
+    generic_test_module_derivative_amount)
 
 from loss import (
     mean_squared_loss)
@@ -13,267 +19,101 @@ from graph_components import (
  
 from main import FullyConnectedModel
 
-def module_test_matrices(times, init_fnc, same_size):
-
-    for _ in range(times):
-        batch_size = randint(low=1, high=3)
-        output_size = randint(low=1, high=3)
-        input_size = randint(low=1, high=3)
-        if same_size:
-            output_size=input_size
-        indiv = int(randint(low=0,high=input_size))
-        inp = rand(batch_size, input_size) * 0.5 + 0.25
-        out = rand(batch_size, output_size) * 0.5 + 0.25
-        model = init_fnc(input_size, output_size)
-        yield batch_size, model, inp, out, indiv
-
-def module_test_matrices_normalized(times, init_fnc, same_size):
-
-    for _ in range(times):
-        batch_size = randint(low=1, high=4)
-        output_size = randint(low=1, high=4)
-        input_size = randint(low=1, high=4)
-        if same_size:
-            output_size=input_size
-        indiv = int(randint(low=0,high=input_size))
-        inp = rand(batch_size, input_size) * 0.5 + 0.25
-        out = rand(batch_size, output_size) * 0.5 + 0.25
-
-        out = out / np.expand_dims(np.sum(out, axis=1), axis=1)
-
-        model = init_fnc(input_size, output_size)
-        yield batch_size, model, inp, out, indiv 
-        
-def module_test_matrices_negative(times, init_fnc, same_size):
-
-    for _ in range(times):
-        batch_size = randint(low=1, high=4)
-        output_size = randint(low=1, high=4)
-        input_size = randint(low=1, high=4)
-        if same_size:
-            output_size=input_size
-        indiv = int(randint(low=0,high=input_size))
-        inp = rand(batch_size, input_size) - 0.5
-        out = rand(batch_size, output_size) - 0.5
-
-        out = out / np.expand_dims(np.sum(out, axis=1), axis=1)
-
-        model = init_fnc(input_size, output_size)
-        yield batch_size, model, inp, out, indiv 
-
-
-def generic_test_module_optimize(init_fnc, loss_fnc, same_size=False):
-    
-    LR = 0.0001
-    tries = 100
-    failures = 0
-    for _, model, inputs, truth, __ in module_test_matrices(tries, init_fnc, same_size):
-        
-        outputs = model.forward(inputs)
-        old_loss, derivative = loss_fnc(prediction=outputs, truth=truth)
-        model.backward(derivative)
-        model.optimize(LR)
-
-        new_outputs = model.forward(inputs)
-        new_loss, derivative = loss_fnc(prediction=new_outputs, truth=truth)
-        if new_loss > old_loss:
-            failures = failures + 1
-    
-    assert failures < tries / 5
-
-def generic_test_module_derivative_direction(
-    init_fnc, loss_fnc, matrices=module_test_matrices, same_size=False):
-    
-    tries = 200
-    LR = 0.0001
-    skips = 0
-    for _, model, inputs, truth, __ in matrices(tries, init_fnc, same_size):
-        
-        outputs = model.forward(inputs)
-        loss, derivative = loss_fnc(prediction=outputs, truth=truth)
-        input_derivative = model.backward(derivative)
-        inputs = inputs - input_derivative * LR
-
-        # Algo breaks down where derivative is mostly flat
-        in_d = input_derivative
-        if np.any(np.abs(in_d) < 0.0025):
-            skips += 1
-
-        new_outputs = model.forward(inputs)
-        new_loss, derivative = loss_fnc(prediction=new_outputs, truth=truth)
-
-        assert new_loss <= loss or (loss == 0)
-
-    assert tries - skips > tries / 20
-
-def generic_test_module_derivative_amount(
-    init_fnc, loss_fnc, matrices=module_test_matrices, same_size=False):
-    '''
-    Tests that altering one element in loss
-    scaled by derivative basically alters
-    the loss by the right amount
-    '''
-    tries = 200
-    skips = 0
-    LR = 0.0000001
-    for batch_size, model, inputs, truth, indiv in matrices(tries, init_fnc, same_size):
-        
-        outputs = model.forward(inputs)
-        old_loss, derivative = loss_fnc(prediction=outputs, truth=truth)
-        input_deriv = model.backward(derivative)
-
-        # Algo breaks down where derivative is mostly flat
-        in_d = input_deriv
-        if np.any(np.abs(in_d) < 0.002):
-            skips += 1
-            continue
-
-        for bs in range(batch_size):
-            change_amount = (1 / input_deriv[bs][indiv]) * LR
-            inputs[bs][indiv] = inputs[bs][indiv] - change_amount
-        
-        new_outputs = model.forward(inputs)
-        new_loss, derivative = loss_fnc(prediction=new_outputs, truth=truth)
-
-        assert np.isclose(
-            (old_loss - new_loss) * (1 / LR),
-            1 * batch_size,
-            atol=0.03) or (old_loss == 0.0 and new_loss == 0.0)
-
-    assert tries - skips > tries / 20
-
 
 def test_MatrixMultModel():
 
-
-    def init(input_size, output_size):
-        return MatrixMult([input_size, output_size])
+    def init(input_shape, output_shape):
+        return MatrixMult([input_shape[1], output_shape[1]])
 
     generic_test_module_optimize(init, mean_squared_loss)
-    generic_test_module_derivative_direction(init, mean_squared_loss)
-    generic_test_module_derivative_amount(init, mean_squared_loss)
+    generic_test_module_derivative_amount(init, mean_squared_loss, shape_generator=alt_generator)
+
 
 def test_MatrixAddModel():
 
-    def init(input_size, output_size):
-        return MatrixAdd([input_size])
+    def init(input_shape, output_shape):
+        return MatrixAdd([input_shape[1]])
 
-    generic_test_module_optimize(init, mean_squared_loss, same_size=True)
-    generic_test_module_derivative_direction(init, mean_squared_loss, same_size=True)
-    generic_test_module_derivative_amount(init, mean_squared_loss, same_size=True)
-
-def test_Relu():
-
-    def init(input_size, output_size):
-        return Relu()
-
-    generic_test_module_derivative_direction(
-        init,
-        mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_negative)
-    
+    generic_test_module_optimize(init, mean_squared_loss, shape_generator=same_generator)
     generic_test_module_derivative_amount(
         init,
         mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_negative)
+        shape_generator=same_generator)
+
+
+def test_Relu():
+
+    def init(input_shape, output_shape):
+        return Relu()
+
+    generic_test_module_derivative_amount(
+        init,
+        mean_squared_loss,
+        shape_generator=same_generator)
+    
 
 def test_Sigmoid():
 
-    def init(input_size, output_size):
+    def init(input_shape, output_size):
         return Sigmoid()
 
-    generic_test_module_derivative_direction(init, mean_squared_loss, same_size=True)
-    generic_test_module_derivative_amount(init, mean_squared_loss, same_size=True)
+    generic_test_module_derivative_amount(
+        init,
+        mean_squared_loss,
+        shape_generator=same_generator)
 
 
 def test_Exponent():
 
-    def init(input_size, output_size):
+    def init(input_shape, output_shape):
         return Exponent()
 
-    generic_test_module_derivative_direction(init, mean_squared_loss, same_size=True)
-    generic_test_module_derivative_amount(init, mean_squared_loss, same_size=True)
+    generic_test_module_derivative_amount(
+        init,
+        mean_squared_loss,
+        shape_generator=same_generator)
 
+    
 def test_LeakyRelu():
 
-    def init(input_size, output_size):
+    def init(input_shape, output_shape):
         return LeakyRelu()
 
-    generic_test_module_derivative_direction(
-        init,
-        mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_negative)
-    
     generic_test_module_derivative_amount(
         init,
         mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_negative)
+        shape_generator=same_generator)
 
+    
 def test_Elo():
 
-    def init(input_size, output_size):
+    def init(input_shape, output_shape):
         return Elo()
     
-    generic_test_module_derivative_direction(
+    generic_test_module_derivative_amount(
         init,
         mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_negative)
+        shape_generator=same_generator)
+
+    
+def test_Probabilize():
+
+    def init(input_shape, output_shape):
+        return Probabilize()
     
     generic_test_module_derivative_amount(
         init,
         mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_negative)
+        shape_generator=same_generator)
 
-def test_Probabilize():
-
-    def init(input_size, output_size):
-        return Probabilize()
-
-    generic_test_module_derivative_direction(
-        init,
-        mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_normalized)
-    generic_test_module_derivative_amount(
-        init,
-        mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_normalized)
-
+    
 def test_Softmax():
 
-    def init(input_size, output_size):
+    def init(input_shape, output_shape):
         return Softmax()
 
-    generic_test_module_derivative_direction(
-        init,
-        mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_normalized)
     generic_test_module_derivative_amount(
         init,
         mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_normalized)
+        shape_generator=same_generator)
 
-def test_Softmax():
-
-    def init(input_size, output_size):
-        return Softmax()
-
-    generic_test_module_derivative_direction(
-        init,
-        mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_normalized)
-    generic_test_module_derivative_amount(
-        init,
-        mean_squared_loss,
-        same_size=True,
-        matrices=module_test_matrices_normalized)
